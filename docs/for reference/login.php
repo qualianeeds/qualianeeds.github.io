6e8151bc11e0689072ee46b8decfc0289bed5660
qualianeeds <98725865+qualianeeds@users.noreply.github.com>
2022-01-30 22:46:44 -0600

    Add files via upload

---
+++ docs/for reference/login.php
@@ -0,0 +1,134 @@
+<!doctype html>
+<html><!-- InstanceBegin template="/Templates/template.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="utf-8">
+<script src="scripts/html5shiv.js"></script>
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Show Pigs for Sale</title>
+<!-- InstanceEndEditable -->
+<!--[if lt IE 9]>
+	<script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<script src="https://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+<![endif]-->
+<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700' rel='stylesheet' type='text/css'>
+<link href="styles.css" rel="stylesheet">
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<!-- optional touchswipe file to enable swipping to navigate slideshow -->
+<script type="text/javascript" src="jquery.touchSwipe.min.js"></script>
+
+<!--/***********************************************
+* Ultimate Fade In Slideshow v2.0- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit Dynamic Drive at https://www.dynamicdrive.com/ for this script and 100s more
+***********************************************/-->
+
+<!--<style>
+
+#fadeshow4 .gallerylayer img{ 
+width: 100%;
+height: auto;
+}
+
+</style>
+
+<script type="text/javascript" src="fadeslideshow.js">
+
+</script>
+
+<script type="text/javascript">
+
+var mygallery=new fadeSlideShow({
+	wrapperid: "fadeshow1", 
+	dimensions: [252, 102], 
+	imagearray: [
+		["images/siouxcenter.jpg", "https://www.facebook.com/Sioux-Center-Swine-AI-616325378390210/"]
+
+	],
+	displaymode: {type:'auto', pause:5000, cycles:0, wraparound:false},
+	persist: false, 
+	fadeduration: 500, 
+	descreveal: "always",
+	togglerid: ""
+})
+</script>-->
+
+</head>
+<body>
+<header> 
+    <section id="container">
+        <article class="c3"> <a href="index.php"><img src="images/graphics/logo2.png" class="logo"></a> </article>
+      <nav>
+                  <a href="terms.php">Terms & Conditions</a> 
+            <span>•</span>   
+            <a href="works.php">How it Works</a> 
+            <span>•</span>   
+            <a href="links.php">Links</a> 
+            <span>•</span>   
+            <a href="contact.php">Contact Us</a>
+              </nav>
+		<!-- floating buttons -->
+      <a href="index.php" id="homebutton"> Home </a>
+      <a href="forsale.php" id="forsale"> For Sale </a>
+                <a href="About Us.php" id="About Us"> About Us </a>
+          <a href="login.php" id="login"> Login </a>
+        </section>
+</header>
+<section id="container">
+<!-- InstanceBeginEditable name="body" -->
+
+<article class="c5 admin">
+
+<h1>Log In</h1>
+<br>
+<form method="post" action="login.php">
+    <input type="hidden" name="login" value="1">
+    <input type="text" name="EM" placeholder="Email (Username)" value="">
+    <input type="password" name="UserPwd" placeholder="Password">
+    <br>
+    <input type="submit" value="submit">
+</form>
+<br>
+<a href="popupemail.php">Forgot your password?</a>
+</article>
+
+
+
+
+
+
+<!-- InstanceEndEditable -->
+	<footer>
+   	  <ul id="footerAds">
+      <li>
+<div class="ad"><a href="tel:4023063645"><img src="ads/pipeline.gif" width="252" height="102" alt=""/></a></div>
+      </li>
+    <li>
+<div class="ad"><a href="tel:4023063645"><img src="ads/advertise-now.jpg" width="252" height="102" alt=""/></a></div>
+      </li>
+
+     <li><div class="ad"><a href="tel:4022091207"><img src="ads/emancipation.gif" width="252" height="102" alt=""/></a></div></li>
+     	</ul>
+      <div class="clear"></div>      
+      <article class="c3">
+      <ul class="footerList">
+       
+        <li><a href="index.php">Home</a></li>
+        <li>• &nbsp;<a href="forsale.php">For Sale</a></li>
+        <li>• &nbsp;<a href="terms.php">Terms/Conditions</a></li>
+        <li>• &nbsp;<a href="works.php">How It Works</a></li>
+        <li>• &nbsp;<a href="links.php">Links</a></li>
+        <li>• &nbsp;<a href="contact.php">Contact</a></li>
+        <li class="small">
+        Website Designed by <a href="https://www.edje.com" target="_blank">EDJE</a> | 
+      Visit the <a href="https://edje.com/directory/index.php?type=Pigs" target="_blank">Pig Page</a></li>
+      </ul>
+      </article>
+    </footer>
+</section>
+<!-- InstanceBeginEditable name="footer" -->
+<!-- InstanceEndEditable -->
+</body>
+<!-- InstanceEnd --></html>
