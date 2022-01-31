7dc0012502180fab2ff2afce59ef260a0cb401c3
qualianeeds <qualianeeds@gmail.com>
2022-01-30 22:22:51 -0600

    Create index.php

---
+++ NF_WEB2/for reference/index.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html><!-- InstanceBegin template="/Templates/template.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="utf-8">
+<script src="scripts/html5shiv.js"></script>
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Show Pigs for Sale</title>
+<!-- InstanceEndEditable -->
+<!--[if lt IE 9]>
+	<script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<script src="https://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+<![endif]-->
+<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700' rel='stylesheet' type='text/css'>
+<link href="styles.css" rel="stylesheet">
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<!-- optional touchswipe file to enable swipping to navigate slideshow -->
+<script type="text/javascript" src="jquery.touchSwipe.min.js"></script>
+
+<!--/***********************************************
+* Ultimate Fade In Slideshow v2.0- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit Dynamic Drive at https://www.dynamicdrive.com/ for this script and 100s more
+***********************************************/-->
+
+<!--<style>
+
+#fadeshow4 .gallerylayer img{ 
+width: 100%;
+height: auto;
+}
+
+</style>
+
+<script type="text/javascript" src="fadeslideshow.js">
+
+</script>
+
+<script type="text/javascript">
+
+var mygallery=new fadeSlideShow({
+	wrapperid: "fadeshow1", 
+	dimensions: [252, 102], 
+	imagearray: [
+		["images/siouxcenter.jpg", "https://www.facebook.com/Sioux-Center-Swine-AI-616325378390210/"]
+
+	],
+	displaymode: {type:'auto', pause:5000, cycles:0, wraparound:false},
+	persist: false, 
+	fadeduration: 500, 
+	descreveal: "always",
+	togglerid: ""
+})
+</script>-->
+
+</head>
+<body>
+<header> 
+    <section id="container">
+        <article class="c3"> <a href="index.php"><img src="images/graphics/logo2.png" class="logo"></a> </article>
+      <nav>
+                  <a href="terms.php">Terms & Conditions</a> 
+            <span>•</span>   
+            <a href="works.php">How it Works</a> 
+            <span>•</span>   
+            <a href="links.php">Links</a> 
+            <span>•</span>   
+            <a href="contact.php">Contact Us</a>
+              </nav>
+		<!-- floating buttons -->
+      <a href="index.php" id="homebutton"> Home </a>
+      <a href="forsale.php" id="forsale"> For Sale </a>
+                <a href="About Us.php" id="About Us"> About Us </a>
+          <a href="login.php" id="login"> Login </a>
+        </section>
+</header>
+<section id="container">
+<!-- InstanceBeginEditable name="body" -->
+	<article class="c5">
+    <h1>WELCOME</h1>
+
+    <p>Welcome to an  alternative way to sell your show pigs, bred gilts, boars and semen.&nbsp; This  website is a listing service that only costs a small fee to list what you want  to sell with absolutely no commissions.&nbsp; The seller is paid directly from  the buyer.&nbsp; Transportation is the buyer&rsquo;s responsibility.&nbsp; The  animals or semen can be listed for as long as the seller chooses with the fee  priced accordingly.&nbsp;&nbsp;&nbsp;</p>
+    <p>Categories will be set up to narrow your search to find what you are  looking in different areas of the country.&nbsp; </p>
+    </article>
+
+
+  	<article class="c3 news">
+    <h1> - What's New -</h1>
+    <h2>SHOW PIGS FOR SALE</h2>
+		<h3>Private Treaty</h3>
+		<h4>All Ages Available</h4>
+    <h4>Crossbreds and purebreds</h4>
+    <h4>Reasonably priced</h4>
+    <h4>Located Plattsmouth, Ne</h4>
+		<!--h3>Selling</h3>
+		<p>• 200 Head Dec/Jan Purbred & Crossbred Pigs<br>
+• 100 Spot Pigs</p>
+  -->
+		<h3>Contact</h3>
+		<p><strong>Doug Wehrbein </strong><br>
+      <a href="tel:4023063648" class="links">402-306-3648</a> • Plattsmouth, NE</p>
+		<p><strong>Kirby Joy, Joy Genetics</strong><br>
+      <a href="tel:4022091207" class="links">402-209-1207</a> • Nebraska City, NE</p>
+
+		<hr>
+    <h4>Sell your Bred Gilts HERE! <br>
+      Contact Us Today.
+      <br>
+      <br>
+      Advertise your business below<br>
+      with your own Banner Ad!<br>
+    </h4>
+  	</article>
+<!-- InstanceEndEditable -->
+	<footer>
+   	  <ul id="footerAds">
+      <li>
+<div class="ad"><a href="tel:4023063645"><img src="ads/pipeline.gif" width="252" height="102" alt=""/></a></div>
+      </li>
+    <li>
+<div class="ad"><a href="tel:4023063645"><img src="ads/advertise-now.jpg" width="252" height="102" alt=""/></a></div>
+      </li>
+
+     <li><div class="ad"><a href="tel:4022091207"><img src="ads/emancipation.gif" width="252" height="102" alt=""/></a></div></li>
+     	</ul>
+      <div class="clear"></div>      
+      <article class="c3">
+      <ul class="footerList">
+       
+        <li><a href="index.php">Home</a></li>
+        <li>• &nbsp;<a href="forsale.php">For Sale</a></li>
+        <li>• &nbsp;<a href="terms.php">Terms/Conditions</a></li>
+        <li>• &nbsp;<a href="works.php">How It Works</a></li>
+        <li>• &nbsp;<a href="links.php">Links</a></li>
+        <li>• &nbsp;<a href="contact.php">Contact</a></li>
+        <li class="small">
+        Website Designed by <a href="https://www.edje.com" target="_blank">EDJE</a> | 
+      Visit the <a href="https://edje.com/directory/index.php?type=Pigs" target="_blank">Pig Page</a></li>
+      </ul>
+      </article>
+    </footer>
+</section>
+<!-- InstanceBeginEditable name="footer" -->
+<!-- InstanceEndEditable -->
+</body>
+<!-- InstanceEnd --></html>
